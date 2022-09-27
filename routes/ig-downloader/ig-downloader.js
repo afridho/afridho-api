e02a972909a2a45ab914b9c8e08d0332bb2629e9
afridho <afridho.utama@gmail.com>
2022-09-27 18:39:30 +0700

    fix

--- routes/ig-downloader/ig-downloader.js
+++ routes/ig-downloader/ig-downloader.js
@@ -54,7 +54,7 @@ async function get_instagram_image(ig_link){
     };
 
     return await axios.request(options).then(function (response) {
-        return ('error' in response.data) ? [response.status, response.data] : [response.status, response.data]
+        return [response.status, response.data]
     }).catch(function (error) {
         if(error.response.status == 429) {
             // || recursion_now <= recursion_limit) {
