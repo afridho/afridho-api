b25e89f7b5da35252adb2c4e2d0bd156b8c23d7c
afridho <afridho.utama@gmail.com>
2022-10-05 04:38:53 +0700

    build

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
 app.use(express.static('frontend/sqlite_viewer'));
 const path = require('path');
 app.use('/sqlite-viewer', function (req, res){
