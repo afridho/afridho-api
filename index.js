466655d02a7597e4f905e2a1acc5bf0bc4899e44
afridho <afridho.utama@gmail.com>
2022-10-05 04:34:25 +0700

    tes

--- index.js
+++ index.js
@@ -17,13 +17,15 @@ app.use(express.json());
 app.use(express.static('client/build'));
 app.use(express.static('frontend/sqlite_viewer'));
 const path = require('path');
+app.use('/sqlite-viewer', function (req, res){
+  res.sendFile(path.resolve(__dirname, 'frontend', 'sqlite_viewer', 'index.html'));
+});
+
 app.get('/', function (req, res){
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-app.get('/sqlite-viewer', function (req, res){
-  res.sendFile(path.resolve(__dirname, 'frontend', 'sqlite_viewer', 'index.html'));
-});
+
 
 app.use("/api/football-update", football_update);
 app.use("/api/fav-football-cron", my_fav_team_cron);
