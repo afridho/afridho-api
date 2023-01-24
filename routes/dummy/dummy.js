b4957c5da81399bca7e3043c5f10a492e5c6d1d6
afridho <afridho.utama@gmail.com>
2023-01-24 19:21:11 +0700

    add dummy analyze spread separate files

--- routes/dummy/dummy.js
+++ routes/dummy/dummy.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const analyzeSpread = require("./json/analyzeSpread")
 require('dotenv').config()
 
 router.get("/", async (req, res) =>{
@@ -8,108 +9,10 @@ router.get("/", async (req, res) =>{
 }
 );
 
+// Analyze Spread Data Dummy
 router.get("/analyze-spread", async (req, res) =>{
     res.status(200)
-    res.json([{
-        "symbol" : "GBPJPY.m",
-        "data": [
-            {
-                "DATE": "2023-01-08",
-                "SYMBOL": "GBPJPY.m",
-                "MODUS": "39",
-                "HIGHEST": "160671",
-                "LOWEST": "160671",
-                "AVERAGE": "39",
-                "NUMBER_OF_TICKS": 1,
-                "VOLATILITY_AVERAGE": 0,
-                "VOLATILITY_POINT": 0,
-                "OPPORTUNITY": 0
-            },
-            {
-                "DATE": "2023-01-09",
-                "SYMBOL": "GBPJPY.m",
-                "MODUS": "39",
-                "HIGHEST": "160671",
-                "LOWEST": "160671",
-                "AVERAGE": "20",
-                "NUMBER_OF_TICKS": 2,
-                "VOLATILITY_AVERAGE": 0,
-                "VOLATILITY_POINT": 0,
-                "OPPORTUNITY": 0
-            },
-            {
-                "DATE": "2023-01-10",
-                "SYMBOL": "GBPJPY.m",
-                "MODUS": "39",
-                "HIGHEST": "160671",
-                "LOWEST": "160671",
-                "AVERAGE": "13",
-                "NUMBER_OF_TICKS": 3,
-                "VOLATILITY_AVERAGE": 0,
-                "VOLATILITY_POINT": 0,
-                "OPPORTUNITY": 0
-            }
-          ],
-        "MODUS_AVG": 11.7,
-        "HIGHEST_AVG": 48201.3,
-        "LOWEST_AVG": 48201.3,
-        "AVERAGE_AVG": 7.2,
-        "NUMBER_OF_TICKS_AVG": 0.6,
-        "VOLATILITY_AVERAGE_AVG": 0,
-        "VOLATILITY_POINT_AVG": 0,
-        "OPPORTUNITY_AVG": 0,
-        "DAYCOUNT": 3
-      },
-      {
-        "symbol" : "AUDUSD",
-        "data": [
-            {
-                "DATE": "2023-01-08",
-                "SYMBOL": "AUDUSD",
-                "MODUS": "39",
-                "HIGHEST": "160671",
-                "LOWEST": "160671",
-                "AVERAGE": "39",
-                "NUMBER_OF_TICKS": 1,
-                "VOLATILITY_AVERAGE": 0,
-                "VOLATILITY_POINT": 0,
-                "OPPORTUNITY": 0
-            },
-            {
-                "DATE": "2023-01-09",
-                "SYMBOL": "AUDUSD",
-                "MODUS": "39",
-                "HIGHEST": "160671",
-                "LOWEST": "160671",
-                "AVERAGE": "20",
-                "NUMBER_OF_TICKS": 2,
-                "VOLATILITY_AVERAGE": 0,
-                "VOLATILITY_POINT": 0,
-                "OPPORTUNITY": 0
-            },
-            {
-                "DATE": "2023-01-10",
-                "SYMBOL": "AUDUSD",
-                "MODUS": "39",
-                "HIGHEST": "160671",
-                "LOWEST": "160671",
-                "AVERAGE": "13",
-                "NUMBER_OF_TICKS": 3,
-                "VOLATILITY_AVERAGE": 0,
-                "VOLATILITY_POINT": 0,
-                "OPPORTUNITY": 0
-            }
-          ],
-        "MODUS_AVG": 11.7,
-        "HIGHEST_AVG": 48201.3,
-        "LOWEST_AVG": 48201.3,
-        "AVERAGE_AVG": 7.2,
-        "NUMBER_OF_TICKS_AVG": 0.6,
-        "VOLATILITY_AVERAGE_AVG": 0,
-        "VOLATILITY_POINT_AVG": 0,
-        "OPPORTUNITY_AVG": 0,
-        "DAYCOUNT": 3
-      }]);
+    res.json(analyzeSpread)
     
 });
 
---
+++ routes/dummy/json/analyzeSpread.js
@@ -0,0 +1,152 @@
+var data = [{
+    "symbol" : "USDJPY",
+    "data": [
+        {
+            "DATE": "2023-01-08",
+            "SYMBOL": "USDJPY",
+            "MODUS": "39",
+            "HIGHEST": "4124",
+            "LOWEST": "21",
+            "AVERAGE": "21",
+            "NUMBER_OF_TICKS": 5,
+            "VOLATILITY_AVERAGE": 21,
+            "VOLATILITY_POINT": 12,
+            "OPPORTUNITY": 12
+        },
+        {
+            "DATE": "2023-01-09",
+            "SYMBOL": "USDJPY",
+            "MODUS": "39",
+            "HIGHEST": "111",
+            "LOWEST": "222",
+            "AVERAGE": "1231",
+            "NUMBER_OF_TICKS": 2,
+            "VOLATILITY_AVERAGE": 0,
+            "VOLATILITY_POINT": 0,
+            "OPPORTUNITY": 0
+        },
+        {
+            "DATE": "2023-01-10",
+            "SYMBOL": "USDJPY",
+            "MODUS": "39",
+            "HIGHEST": "231",
+            "LOWEST": "2131",
+            "AVERAGE": "2313",
+            "NUMBER_OF_TICKS": 6,
+            "VOLATILITY_AVERAGE": 0,
+            "VOLATILITY_POINT": 0,
+            "OPPORTUNITY": 0
+        }
+        ],
+    "MODUS_AVG": 11.7,
+    "HIGHEST_AVG": 48201.3,
+    "LOWEST_AVG": 48201.3,
+    "AVERAGE_AVG": 7.2,
+    "NUMBER_OF_TICKS_AVG": 0.6,
+    "VOLATILITY_AVERAGE_AVG": 0,
+    "VOLATILITY_POINT_AVG": 0,
+    "OPPORTUNITY_AVG": 0,
+    "DAYCOUNT": 3
+    },
+    {
+"symbol" : "GBPJPY.m",
+"data": [
+    {
+        "DATE": "2023-01-08",
+        "SYMBOL": "GBPJPY.m",
+        "MODUS": "39",
+        "HIGHEST": "160671",
+        "LOWEST": "160671",
+        "AVERAGE": "39",
+        "NUMBER_OF_TICKS": 1,
+        "VOLATILITY_AVERAGE": 0,
+        "VOLATILITY_POINT": 0,
+        "OPPORTUNITY": 0
+    },
+    {
+        "DATE": "2023-01-09",
+        "SYMBOL": "GBPJPY.m",
+        "MODUS": "39",
+        "HIGHEST": "160671",
+        "LOWEST": "160671",
+        "AVERAGE": "20",
+        "NUMBER_OF_TICKS": 2,
+        "VOLATILITY_AVERAGE": 0,
+        "VOLATILITY_POINT": 0,
+        "OPPORTUNITY": 0
+    },
+    {
+        "DATE": "2023-01-10",
+        "SYMBOL": "GBPJPY.m",
+        "MODUS": "39",
+        "HIGHEST": "160671",
+        "LOWEST": "160671",
+        "AVERAGE": "13",
+        "NUMBER_OF_TICKS": 3,
+        "VOLATILITY_AVERAGE": 0,
+        "VOLATILITY_POINT": 0,
+        "OPPORTUNITY": 0
+    }
+    ],
+"MODUS_AVG": 11.7,
+"HIGHEST_AVG": 48201.3,
+"LOWEST_AVG": 48201.3,
+"AVERAGE_AVG": 7.2,
+"NUMBER_OF_TICKS_AVG": 0.6,
+"VOLATILITY_AVERAGE_AVG": 0,
+"VOLATILITY_POINT_AVG": 0,
+"OPPORTUNITY_AVG": 0,
+"DAYCOUNT": 3
+},
+{
+"symbol" : "AUDUSD",
+"data": [
+    {
+        "DATE": "2023-01-08",
+        "SYMBOL": "AUDUSD",
+        "MODUS": "39",
+        "HIGHEST": "160671",
+        "LOWEST": "160671",
+        "AVERAGE": "39",
+        "NUMBER_OF_TICKS": 1,
+        "VOLATILITY_AVERAGE": 0,
+        "VOLATILITY_POINT": 0,
+        "OPPORTUNITY": 0
+    },
+    {
+        "DATE": "2023-01-09",
+        "SYMBOL": "AUDUSD",
+        "MODUS": "39",
+        "HIGHEST": "160671",
+        "LOWEST": "160671",
+        "AVERAGE": "20",
+        "NUMBER_OF_TICKS": 2,
+        "VOLATILITY_AVERAGE": 0,
+        "VOLATILITY_POINT": 0,
+        "OPPORTUNITY": 0
+    },
+    {
+        "DATE": "2023-01-10",
+        "SYMBOL": "AUDUSD",
+        "MODUS": "39",
+        "HIGHEST": "160671",
+        "LOWEST": "160671",
+        "AVERAGE": "13",
+        "NUMBER_OF_TICKS": 3,
+        "VOLATILITY_AVERAGE": 0,
+        "VOLATILITY_POINT": 0,
+        "OPPORTUNITY": 0
+    }
+    ],
+"MODUS_AVG": 11.7,
+"HIGHEST_AVG": 48201.3,
+"LOWEST_AVG": 48201.3,
+"AVERAGE_AVG": 7.2,
+"NUMBER_OF_TICKS_AVG": 0.6,
+"VOLATILITY_AVERAGE_AVG": 0,
+"VOLATILITY_POINT_AVG": 0,
+"OPPORTUNITY_AVG": 0,
+"DAYCOUNT": 3
+}]
+
+module.exports = data;
