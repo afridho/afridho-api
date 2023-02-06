75c18fdcebb87912ebd72c4e7d4a442b5d42d7cb
afridho <afridho.utama@gmail.com>
2023-02-06 18:29:41 +0700

    add dummy 'dashboard' api

--- routes/dummy/dummy.js
+++ routes/dummy/dummy.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const analyzeSpread = require("./json/analyzeSpread")
+const dashboard = require("./json/dashboard")
 require('dotenv').config()
 
 router.get("/", async (req, res) =>{
@@ -13,6 +14,13 @@ router.get("/", async (req, res) =>{
 router.get("/analyze-spread", async (req, res) =>{
     res.status(200)
     res.json(analyzeSpread)
+    
+});
+
+// CEO > Dashboard Dummy
+router.get("/dashboard", async (req, res) =>{
+    res.status(200)
+    res.json(dashboard)
     
 });
 
