fa338ebdf16db5aaa71e512fb6841bc9f44d0f85
afridho <afridho.utama@gmail.com>
2023-02-16 02:56:25 +0700

    Dummy > Net Position Dialog (add data)

--- routes/dummy/json/NetPositionDialog.js
+++ routes/dummy/json/NetPositionDialog.js
@@ -28,7 +28,23 @@ var data = {
             "created_at": "2023-02-08 01:11:07",
             "updated_at": "2023-02-14 10:09:28",
             "snapshot": []
-        }],
+        },
+        {
+          "id": 3,
+          "email": "apple@icloud.com",
+          "login": 23123,
+          "group_server": "Surya",
+          "server": "MT4IN1",
+          "symbol": "XAUUSD.m",
+          "cmd": 1,
+          "net_position": 4.33,
+          "active_lot": 0,
+          "pending_lot": 0,
+          "created_at": "2023-02-08 01:11:07",
+          "updated_at": "2023-02-14 10:09:28",
+          "snapshot": []
+      }
+      ],
         "message": "Successfully",
         "status": "success"
 }
