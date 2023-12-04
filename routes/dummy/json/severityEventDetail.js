c9ddaf13e7c67d2be107ea8cb7903da631789a43
afridho <afridho.utama@gmail.com>
2023-12-04 17:02:53 +0700

    push

---
+++ routes/dummy/json/severityEventDetail.js
@@ -0,0 +1,14 @@
+var data = [
+    {
+        account: 99007743,
+        events: ['Mahi - 30s', 'Biglot', 'AV Profit'],
+        status: 'Pantau',
+    },
+    {
+        account: 99120378,
+        events: ['Biglot', 'PL Yesterday', 'PL Today'],
+        status: 'Aman',
+    },
+];
+
+module.exports = data;
